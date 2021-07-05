41e564410637257baca6e5261780740c79ff9f10
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2021-07-05 18:39:34 -0300

    init

---
+++ ListaDAO.js
@@ -0,0 +1,33 @@
+function ListaDAO() {
+    var listas = {};
+
+    this.criarLista = function (nome) {
+        //gerar id
+        var novoID = "lista-" + Object.keys(this.listas).length;
+        //criar objeto da lista
+        var novaLista = {
+            id: novoID,
+            nome: nome,
+            tarefas: [],
+            numeroDeTarefas: 0
+        };
+        //adicionar nova lista ao objeto listas
+        this.listas[novoID] = novaLista;
+
+        return{
+            sucess: true,
+            message: "Lista \'" + nome + "\' criada"
+        }
+    };
+    
+    this.renomearLista = function () {};
+    this.apagarLista = function () {};
+    this.getListas = function () {};
+    this.novaTarefa = function () {};
+    this.toggleTarefa = function () {};
+    this.apagarTarefa = function () {};
+    this.getTarefas = function () {};
+
+}
+
+module.exports = new ListaDAO();
