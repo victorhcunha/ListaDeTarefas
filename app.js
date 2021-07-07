00338bfa125beece4b461a9ffdef6d11a70802d1
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2021-07-07 01:02:07 -0300

    app teste

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ var express = require("express");
 
 var app = express();
 
+app.get("/teste", function (request, response) {
+    console.log("Rota /teste acessada com sucesso!");
+});
+
+app.listen(8080);
