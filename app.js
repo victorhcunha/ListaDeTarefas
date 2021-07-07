5195534419591490ca647522bb1ce69803c44bdd
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2021-07-07 00:09:12 -0300

    criar apagar e renomear lista

--- app.js
+++ app.js
@@ -10,6 +10,44 @@ var app = express();
 app.use(bodyParser.json());
 
 //rotas
+// nova lista
+app.post("/lista", function(request, response) {
+    //pega o nome da propriedade body do request
+    var nome = request.body,nome;
+    //cria nova lista
+    var result = listaDao.criarLista(nome);
+
+    response.status(200);
+    response.json(result);
+    response.end();
+});
+
+//renomear lista
+app.put("/lsita/:id_lista", function (request, response) {
+    //pega o nome da propriedade body do request
+    var novoNome = request.body.nome;
+    //pega o id da lista dos parâmetros da rota
+    var idDaLista = request.params.id_lista;
+    //renomeia a lista
+    var result = listaDao.renomearLista(idDaLista, novoNome);
+    response.status(200);
+    response.json(result);
+    response.end();
+});
+
+//apagar lista
+app.delete("/lista/:id_lista", function (request, response) {
+    //pega o nome da propriedade body do request
+    var idDaLista = request.params.id_lista;
+    //apaga a lista com o identificador correspondente
+    var result = listaDao.apagarLista(idDaLista);
+    response.status(200);
+    response.json(result);
+    response.end();
+});
+
+
+
 
 //associar api com porta 8080
 app.listen(8080)
